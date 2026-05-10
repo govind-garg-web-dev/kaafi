a41ca86f03062211a63589c83863ffa103d58a5d
govind-garg-web-dev <138015609+govind-garg-web-dev@users.noreply.github.com>
2026-05-10 14:09:23 +0530

    Add Sentry DSN to client and server config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: "https://21ddf9b54bd3cdcdc049cd195f1ecc4a@o4511364629921792.ingest.de.sentry.io/4511364680777808",
   tracesSampleRate: 0.2,
   debug: false,
   replaysOnErrorSampleRate: 1.0,
